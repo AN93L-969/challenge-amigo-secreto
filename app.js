f16e0d47bcd9fe9cc561d71e2914f689e93d0d1c
Angel Melendez <angel99.melendez@gmail.com>
2025-08-23 00:33:47 -0600

    Adicionando la funcion actualizar lista y comentarios importantes.

--- app.js
+++ app.js
@@ -9,11 +9,28 @@ de dar click en el boton "Añadir", al finalizar se limpia el campo de texto.
 function agregarAmigo() {
     let amigo = document.getElementById("amigo").value;
     
-    if (amigo == "") {        
+    if (amigo == "") {  
+        //Mostrara un mensaje de alerta si el campo esta vacio      
         alert("Por favor, ingrese un nombre.");
 
     } else {
+        //Sino, se agregara el nombre al array.
         amigos.push(amigo);
+        actualizarLista(amigos); //Se implementa la función para actualizar la lista en el HTML.
         document.getElementById("amigo").value = "";
+    }
+}
+
+/* 
+El objetivo de la siguiente función es actualizar la lista de los amigos
+cada vez que el usuario ingrese un nuevo nombre, para ello se recorre el array
+y se va agregando cada nombre a la lista en el HTML.
+*/
+function actualizarLista(lista) {
+    let contenidoLista = document.getElementById("listaAmigos");
+    contenidoLista.innerHTML = ""; //Se limpia la lista antes de actuañizar para evitar redundancias.
+
+    for (let i = 0; i < lista.length; i++) {
+        contenidoLista.innerHTML += `<li>${lista[i]}</li>`;
     }
 }
