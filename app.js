caf515c3a8d7a9f465f1742b48413c5d5414610e
Angel Melendez <angel99.melendez@gmail.com>
2025-08-22 23:42:49 -0600

    Creación de la variable amigos y creación de la función agregarAmigo.

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []; //Esta varibale alamecenara los nombres de los amigos que ingresara el ususario.
+
+/* 
+La siguiente función tiene como objetivo agregar el nombre que ingrese el usuario
+al array "amigos", y a su vez valida que el campo no este vacio antes
+de dar click en el boton "Añadir", al finalizar se limpia el campo de texto.
+*/
+function agregarAmigo() {
+    let amigo = document.getElementById("amigo").value;
+    
+    if (amigo == "") {        
+        alert("Por favor, ingrese un nombre.");
+
+    } else {
+        amigos.push(amigo);
+        document.getElementById("amigo").value = "";
+    }
+}
